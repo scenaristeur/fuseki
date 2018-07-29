fa9988d4f5ee4f868d5ae0ca6c4282389502b2b2
David <scenaristeur@gmail.com>
2018-07-29 21:08:02 +0200

    Update install.js
    wget & tar for windows .
    must now find a way to change   this.process = spawn('bash', [script], {env: environment}) in index.js

--- install.js
+++ install.js
@@ -1,6 +1,8 @@
 /* global exec, mkdir, mv, test */
 
-require('shelljs/global')
+require('shelljs/global');
+var wget = require('node-wget');
+var tar = require('tar')
 
 var path = require('path')
 
@@ -11,10 +13,37 @@ var fusekiApp = path.join(__dirname, '/../../fuseki')
 
 if (!test('-f', fusekiDownload)) {
   mkdir('-p', 'downloads')
-  exec('wget https://mirror.synyx.de/apache/jena/binaries/' + fusekiTar + ' -O "' + fusekiDownload + '"')
+//  exec('wget https://mirror.synyx.de/apache/jena/binaries/' + fusekiTar + ' -O "' + fusekiDownload + '"')
+var url = 'https://mirror.synyx.de/apache/jena/binaries/'+fusekiTar;
+
+//var url = "https://spoggy.herokuapp.com/";
+wget({url: url, dest: fusekiDownload},    function (error, response, body) {
+        if (error) {
+            console.log('--- error:');
+            console.log(error);            // error encountered
+        } else {
+            console.log('--- headers:');
+            console.log(response.headers); // response headers
+            console.log('--- body:');
+            console.log(body);             // content of package
+            fin()
+        }
+    });
 }
 
+
+function fin(){
+  console.log("Fuseki Téléchargé")
+
 if (!test('-d', fusekiApp)) {
-  exec('tar -xzf ' + fusekiDownload)
+  //exec('tar -xzf ' + fusekiDownload)
+  tar.x(  // or tar.extract(
+  {
+    file: fusekiDownload
+  }
+).then(_=> {
   mv(fusekiUnzip, fusekiApp)
-}
+   //.. tarball has been dumped in cwd .. })
+console.log("Installation de Fuseki dans "+fusekiApp+" terminéee")
+});
+}}
