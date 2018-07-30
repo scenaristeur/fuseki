bf9c19fc70ec7691fc89c8d5dbcd194cf6f6a17f
David <scenaristeur@gmail.com>
2018-07-30 09:01:00 +0200

    Update install.js

--- install.js
+++ install.js
@@ -11,36 +11,47 @@ var fusekiDownload = path.join(__dirname, 'downloads', fusekiTar)
 var fusekiUnzip = path.join(__dirname, fusekiTar.split('.').slice(0, -2).join('.'))
 var fusekiApp = path.join(__dirname, '/../../fuseki')
 
+var platform = process.platform
+
+
 if (!test('-f', fusekiDownload)) {
   mkdir('-p', 'downloads')
-var url = 'https://mirror.synyx.de/apache/jena/binaries/'+fusekiTar;
+  var url = 'https://mirror.synyx.de/apache/jena/binaries/'+fusekiTar;
 
-//var url = "https://spoggy.herokuapp.com/";
-wget({url: url, dest: fusekiDownload},    function (error, response, body) {
-        if (error) {
-            console.log('--- error:');
-            console.log(error);            // error encountered
-        } else {
-            console.log('--- headers:');
-            console.log(response.headers); // response headers
-            console.log('--- body:');
-            console.log(body);             // content of package
-            untar_fuseki()
-        }
+  //var url = "https://spoggy.herokuapp.com/";
+
+
+
+  if (platform == 'linux'){
+    exec('wget https://mirror.synyx.de/apache/jena/binaries/' + fusekiTar + ' -O "' + fusekiDownload + '"')
+    untar_fuseki();
+  }else{
+    wget({url: url, dest: fusekiDownload},    function (error, response, body) {
+      if (error) {
+        console.log('--- error:');
+        console.log(error);            // error encountered
+      } else {
+        console.log('--- headers:');
+        console.log(response.headers); // response headers
+        console.log('--- body:');
+        console.log(body);             // content of package
+        untar_fuseki()
+      }
     });
+  }
 }
 
 
 function untar_fuseki(){
   console.log("Fuseki Downloaded")
 
-if (!test('-d', fusekiApp)) {
-  tar.x(  // or tar.extract(
-  {
-    file: fusekiDownload
-  }
-).then(_=> {
-  mv(fusekiUnzip, fusekiApp)
-console.log("Fuseki installed in "+fusekiApp)
-});
-}}
+  if (!test('-d', fusekiApp)) {
+    tar.x(  // or tar.extract(
+      {
+        file: fusekiDownload
+      }
+    ).then(_=> {
+      mv(fusekiUnzip, fusekiApp)
+      console.log("Fuseki installed in "+fusekiApp)
+    });
+  }}
